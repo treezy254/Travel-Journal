289a6575d29cf1a5e69f22e7758d2ae21197ea8b
Pain Killer <gachungasamuel53@gmail.com>
2022-06-18 21:49:40 +0300

    aloha

---
+++ src/components/content/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./content.scss"
+
+export const Content = () => {
+  return (
+    <div className='content'>
+        <h2>About</h2>
+        <p>I'm a frontend developer witha particular intrest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+
+        <h2>Interests</h2>
+        <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Enterprenuer. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </div>
+  )
+}
--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import Main from '../../components/main/Main'
+import Header from '../../components/header/Header'
+import { Content } from '../../components/content/Content'
+import Footer from '../../components/footer/Footer'
 import "./home.scss"
 
 const Home = () => {
   return (
     <div>
-        <Navbar />
-        <Main />
+        <Header />
+        <Content />
+        <Footer />
     </div>
   )
 }
